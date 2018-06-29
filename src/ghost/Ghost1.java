d65ade1b48f0d076cb5be6eb7ca7b2dde785f4f8
CLAB_36 <CLAB_36@DESKTOP-1IJP48P>
2018-06-29 10:45:59 +0530

    initial

---
+++ src/ghost/Ghost1.java
@@ -0,0 +1,26 @@
+
+package ghost;
+
+
+
+public class Ghost1 {
+int num1=9000;
+String b="Bleh Bleh Bleh";
+    
+    public static void add(int a,int b)
+    {
+        int c=a+b;
+        System.out.println(""+c);
+    }
+    
+    
+
+    public static void main(String[] args)
+    {
+        Ghost1 j1=new Ghost1();
+        System.out.println(j1.num1);
+        System.out.println(j1.b);
+    add(123,200); 
+    }
+    
+}
