4f01b81e99048d0fc1e372f4d351fe85bfeef2bb
CLAB_36 <CLAB_36@DESKTOP-1IJP48P>
2018-07-02 10:39:32 +0530

    initial

---
+++ src/ghost/Ghost2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ghost;
+
+/**
+ *
+ * @author CLAB_36
+ */
+
+public class Ghost2 {
+    
+    String name="LED MONITOR";
+    double price=2000000;
+    int quantity =10;
+    
+    void changeName(String newName)
+    {
+    name=newName;    
+    }
+    void changePrice(double newPrice)
+    {
+    price=newPrice;    
+    }
+    void changeQuantity(int newQuantity)
+    {
+    quantity=newQuantity;    
+    }
+    
+     public static void main(String[] args)
+    {
+        
+    }
+    
+}
