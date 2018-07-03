0e1b1befdd6f7109197950cf50183abe60de0b53
CLAB_36 <CLAB_36@DESKTOP-1IJP48P>
2018-07-03 10:51:14 +0530

    initial

--- src/ghost/Ghost2.java
+++ src/ghost/Ghost2.java
@@ -30,13 +30,29 @@ public class Ghost2 {
     }
     void printChanges()
     {
-    System.out.println("Product: "+name+",Price: "+price+", Quantity: "+quantity);    
+    System.out.println("Product: "+name+" , Price: "+price+" , Quantity: "+quantity);    
     }
 
     public static void main(String[] args)
     {
         Ghost2 name = new Ghost2();
         name.printChanges();
+    
+        //Create 2 Product Objects
+        Ghost2 product1=new Ghost2();
+        Ghost2 product2=new Ghost2();
+        
+        //Invoke method on each objects
+        product1.changeName("Keyboard");
+        product1.changePrice(85000);
+        product1.changeQuantity(100);
+        product1.printChanges();
+    
+        product2.changeName("Mouse");
+        product2.changePrice(55000);
+        product2.changeQuantity(120);
+        product2.printChanges();    
     }
     
 }
+        
